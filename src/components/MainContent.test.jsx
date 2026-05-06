0b67a60f25cfde288df29ab951d8e6da8e55651d
chcamp <c_campos_cca@hotmail>
2026-05-06 18:56:48 -0500

    added comment 4

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
-//My Test tres
+//My Test 4
 
 describe('MainContent', () => {
   it('should render a button', () => {
